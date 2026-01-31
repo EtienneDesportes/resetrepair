83269218060afccf07a3ef60ad092e4b6812e64d
EtienneDesportes <74676239+EtienneDesportes@users.noreply.github.com>
2026-01-31 18:35:33 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,93 @@ export default function Document() {
             `,
           }}
         />
+        
+        {/* Schema.org LocalBusiness Markup */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "@id": "https://www.resetlab.fr/#organization",
+              "name": "Reset Lab",
+              "image": "https://www.resetlab.fr/images/logo-reset-lab.jpg",
+              "description": "Expert en réparation électronique à Schiltigheim. Spécialiste micro-soudure sur Console, Macbook, Carte-Mère, PC, Audio, etc...",
+              "url": "https://www.resetlab.fr",
+              "telephone": "+33678235040",
+              "priceRange": "€",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "21 rue du Soleil",
+                "addressLocality": "Schiltigheim",
+                "postalCode": "67300",
+                "addressRegion": "Grand Est",
+                "addressCountry": "FR"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": "48.607532",
+                "longitude": "7.756656"
+              },
+              "openingHoursSpecification": [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                  "opens": "09:00",
+                  "closes": "20:00"
+                }
+              ],
+              "areaServed": [
+                {
+                  "@type": "City",
+                  "name": "Schiltigheim"
+                },
+                {
+                  "@type": "City",
+                  "name": "Strasbourg"
+                },
+                {
+                  "@type": "City",
+                  "name": "Bischheim"
+                },
+                {
+                  "@type": "City",
+                  "name": "Hoenheim"
+                }
+              ],
+              "hasOfferCatalog": {
+                "@type": "OfferCatalog",
+                "name": "Services de réparation",
+                "itemListElement": [
+                  {
+                    "@type": "Offer",
+                    "itemOffered": {
+                      "@type": "Service",
+                      "name": "Réparation par micro-soudure",
+                      "description": "Réparation carte mère MacBook, consoles, PC, Enceinte, Audio par micro-soudure"
+                    }
+                  },
+                  {
+                    "@type": "Offer",
+                    "itemOffered": {
+                      "@type": "Service",
+                      "name": "Réparation console de jeux",
+                      "description": "Réparation PlayStation, Xbox, Nintendo Switch"
+                    }
+                  },
+                  {
+                    "@type": "Offer",
+                    "itemOffered": {
+                      "@type": "Service",
+                      "name": "Réparation ordinateurs",
+                      "description": "Réparation MacBook, PC portables, cartes mères"
+                    }
+                  }
+                ]
+              }
+            })
+          }}
+        />
       </Head>
       <body>
         <Main />
